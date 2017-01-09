bde64eea8d2f5e1cccf9cf5569b32bb86b8a84b6
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-01-09 01:31:14 -0800

    support remote connection

--- src/main/cc/switch.h
+++ src/main/cc/switch.h
@@ -1,4 +1,7 @@
 #include "simpleswitchcore.h"
+#include "channel.h"
+
+#define __SWITCH__
 
 struct switch_data_t {
   struct {
@@ -11,7 +14,19 @@ struct switch_data_t {
 class switch_t
 {
 public:
-  switch_t(simif_t* sim): sim(sim), sw(init_switch(1)) { }
+  switch_t(simif_t* sim): sim(sim),
+#ifdef __SWITCH__
+    in("s_in"), out("s_out")
+  {
+    out.release();
+    in.release();
+  }
+#else
+    sw(init_switch(1)) { }
+#endif
+
+  ~switch_t() {
+  }
 
   void send(switch_data_t& data) {
     if (!data.in.value.is_empty) {
@@ -35,6 +50,18 @@ public:
   }
 
   void tick(switch_data_t& data) {
+#ifdef __SWITCH__
+if (!data.out.value.is_empty) {
+  printf("data out: %llx, last: %d\n", data.out.value.data, data.out.value.is_last);
+  fflush(stdout);
+}
+    send_switch(data.out.value);
+    recv_switch(data.in.value); 
+if (!data.in.value.is_empty) {
+  printf("data in: %llx, last: %d\n", data.in.value.data, data.in.value.is_last);
+  fflush(stdout);
+}
+#else
     transport_value value;
     value.data = data.out.value.data;
     value.is_last = data.out.value.is_last;
@@ -43,9 +70,45 @@ public:
     data.in.value.data = value.data;
     data.in.value.is_last = value.is_last;
     data.in.value.is_empty = value.is_empty;
+#endif
   }
 
 private:
   simif_t* sim;
+#ifdef __SWITCH__
+  channel_t in;
+  channel_t out;
+
+  void send_switch(transport_value& value) {
+    bool ready;
+    do {
+      in.acquire();
+      if ((ready = in.ready())) {
+	in[0] = (value.data >> 32) & 0xffffffff;
+        in[1] = value.data & 0xffffffff;
+        in[2] = value.is_last;
+        in[3] = value.is_empty;
+	in.produce();
+      }
+      in.release();
+    } while(!ready);
+  }
+
+  void recv_switch(transport_value& value) {
+    bool valid;
+    do {
+      out.acquire();
+      if ((valid = out.valid())) {
+        value.data = ((uint64_t)out[0] << 32) | out[1];
+        value.is_last = out[2];
+        value.is_empty = out[3];
+	out.consume();
+      }
+      out.release();
+    } while(!valid);
+  }
+
+#else
   simpleswitch* sw;
+#endif
 };
