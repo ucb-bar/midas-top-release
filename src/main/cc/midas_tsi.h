79452dc4a1c5799bcbcf1015fc5ab7dc6e804486
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-01-26 11:59:01 -0800

    fix midas_tsi

--- src/main/cc/midas_tsi.h
+++ src/main/cc/midas_tsi.h
@@ -10,7 +10,6 @@ class midas_tsi_t : public fesvr_proxy_t, public midas_fesvr_t
  public:
   midas_tsi_t(const std::vector<std::string>& args);
   virtual ~midas_tsi_t();
-  virtual void wait();
   virtual void tick();
 
   virtual bool recv_mem_req(fesvr_mem_t& req);
@@ -20,11 +19,11 @@ class midas_tsi_t : public fesvr_proxy_t, public midas_fesvr_t
   virtual bool recv_loadmem_req(fesvr_loadmem_t& loadmem);
   virtual void recv_loadmem_data(void* buf, size_t len);
 
+  virtual bool busy() {
+    return midas_fesvr_t::busy();
+  }
   virtual bool done() {
     return midas_fesvr_t::done();
-  }
-  virtual bool started() {
-    return midas_fesvr_t::started();
   }
   virtual int exit_code() {
     return midas_fesvr_t::exit_code();
@@ -34,6 +33,7 @@ class midas_tsi_t : public fesvr_proxy_t, public midas_fesvr_t
   midas_context_t host;
   midas_context_t* target;
 
+  virtual void wait();
   static int host_thread(void *tsi);
 };
 
