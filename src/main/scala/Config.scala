4a7f457b23559d3305c4208651a3678132fac4ad
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-03-05 22:17:52 -0800

    bump rocketchip for config changes

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -6,27 +6,21 @@ import dram_midas._
 
 class ZynqConfigWithMemModel extends Config(new WithLBPipe ++ new ZynqConfig)
 
-class WithLBPipe extends Config(
-  (key, _, _) => key match {
-    case MemModelKey => Some((p: Parameters) => new MidasMemModel(
-      new LatencyPipeConfig(new BaseParams(maxReads = 16, maxWrites = 16)))(p))
-  }
-)
+class WithLBPipe extends Config((site, here ,up) => {
+  case MemModelKey => Some((p: Parameters) => new MidasMemModel(
+    new LatencyPipeConfig(new BaseParams(maxReads = 16, maxWrites = 16)))(p))
+})
 
-class NoDebug extends Config(
-  (key, _, _) => key match {
-    case rocket.UseDebug => false
-  }
-)
+class NoDebug extends Config((site, here, up) => {
+  case rocket.UseDebug => false
+})
 
 class DefaultExampleConfig extends Config(new NoDebug ++ new rocketchip.BaseConfig)
 /*
 class SmallBOOMConfig extends Config(new NoBrPred ++ new NoDebug ++ new boom.SmallBOOMConfig)
 
-class NoBrPred extends Config(
-  (key, _, _) => key match {
-    case boom.EnableBranchPredictor => false
-  }
-)
+class NoBrPred extends Config((site, here, up) => {
+  case boom.EnableBranchPredictor => false
+})
 */
 class RocketChip1GExtMem extends Config(new rocketchip.WithExtMemSize(0x40000000L) ++ new rocketchip.BaseConfig)
